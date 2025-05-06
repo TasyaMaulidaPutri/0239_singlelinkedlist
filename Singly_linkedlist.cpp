dc9aaa26acf59fef078cb29564dd7953b7dff1c5
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-05-06 12:14:18 +0700

    add prosses traverse

--- Singly_linkedlist.cpp
+++ Singly_linkedlist.cpp
@@ -86,4 +86,24 @@ public:
         delete current;
         return true;
     }
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kososng\n";
+        }
+        else
+        {
+            cout << "\nData didalam list adalah:\n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
     
+};
+
